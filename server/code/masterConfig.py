f725c8ec6f1adbbf188a7444d4ad8f3590cd977b
Your Name <you@example.com>
2016-07-02 04:09:22 +0530

    eisamay

--- server/code/masterConfig.py
+++ server/code/masterConfig.py
@@ -1,6 +1,6 @@
 import hashlib
 #All Master configurations.
 mconfig={
-    'allowled':['eisamay'],#'sangbadpratidin','zeenews','anandabazar'],
+    'allowled':['eisamay','sangbadpratidin','zeenews','anandabazar'],
     'max_news_in_each_cata':10,
 }
