310960c048d9892e1ecdf066d600249aa0eab364
Dipankar Dutta <ddutta@microsoft.com>
2016-08-25 22:30:31 +0530

    Auto Checkin using autocheckin.bat

--- client/app/src/main/java/in/peerreview/flipnews/Activities/MainApplication.java
+++ client/app/src/main/java/in/peerreview/flipnews/Activities/MainApplication.java
@@ -2,13 +2,23 @@ package in.peerreview.flipnews.Activities;
 
 import android.app.Application;
 import android.content.Intent;
+import android.os.AsyncTask;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import in.peerreview.flipnews.Configarations.GlobalAppConfigurations;
 import in.peerreview.flipnews.Utils.L;
 import in.peerreview.flipnews.Utils.SimpleUtils;
 
@@ -16,6 +26,7 @@ import in.peerreview.flipnews.Utils.SimpleUtils;
  * Created by ddutta on 8/25/2016.
  */
 public class MainApplication extends Application {
+
     @Override
     public void onCreate ()
     {
@@ -47,11 +58,45 @@ public class MainApplication extends Application {
             res.put("Exception_pinToFile",e.getCause ().getStackTrace()[0].getFileName());
             res.put("Exception_pinToLine",e.getCause ().getStackTrace()[0].getLineNumber());
             L.e(res.toString());
+            new SendReportToServer().execute(res);
+            //sendToServer(res);
         } catch (JSONException e1) {
             e1.printStackTrace();
         }
 
         L.d("##############################  handleUncaughtException  ###################################");
+
+    }
+
+}
+
+class SendReportToServer extends AsyncTask<JSONObject, Void, Void> {
+
+    private Exception exception;
+
+    @Override
+    protected Void doInBackground(JSONObject... data) {
+        try {
+            sendToServer(data[0]);
+        } catch (Exception e) {
+            this.exception = e;
+        }
         System.exit(1); // kill off the crashed app
+        return null;
+    }
+    private void sendToServer(JSONObject jsonObject) throws IOException {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(GlobalAppConfigurations.BACKEND_REPORT_URL);
+            String json = jsonObject.toString();
+            StringEntity se = new StringEntity(json);
+            httpPost.setEntity(se);
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+            InputStream inputStream = httpResponse.getEntity().getContent();
+            if(inputStream != null)
+                L.d("Success");
+            else
+                L.d("Failes to upload crash report");
     }
 }
--- client/app/src/main/java/in/peerreview/flipnews/Configarations/GlobalAppConfigurations.java
+++ client/app/src/main/java/in/peerreview/flipnews/Configarations/GlobalAppConfigurations.java
@@ -7,5 +7,6 @@ public class GlobalAppConfigurations {
 
     //public static final String BACKEND_API_URL = "http://peerreview.in:5555/news";
     public static final String BACKEND_API_URL = "http://192.168.0.8:5555/news";
+    public static final String BACKEND_REPORT_URL = "http://192.168.0.8:5555/reports";
 
 }
